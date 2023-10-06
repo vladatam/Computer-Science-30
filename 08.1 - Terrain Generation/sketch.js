b9925a88504cb0526f11163cd81c1ce0c68faa73
vladatam <vladyslav.atam@gmail.com>
2023-10-06 10:44:25 -0600

    Update sketch.js

--- 08.1 - Terrain Generation/sketch.js
+++ 08.1 - Terrain Generation/sketch.js
@@ -13,31 +13,52 @@ const noiseShift = 0.01;
 
 function randomTerrain(){
   let noiseReduction = height;
-  rectMode(CORNERS);
   let tallest = 0;
+  let averageCalc = 0;
+
+  rectMode(CORNERS);
+
   strokeWeight(lineWidth);
-  while(x < width){
-    line(x,height,x,height);
-    //option 1 
+
+  for(let i=0; i < width; i++){
+    x += segmentLength;
     let heightRect = random(height);
     //Noise reduction
+
     heightRect = noise(noiseReduction); // 0 to 1
     heightRect = map(heightRect,0,1,0,height);
     noiseReduction += noiseShift; 
-
+    //Render the mountains
     rect(x,height,x,heightRect);
 
-    stroke("red");
-    line(0,heightRect,width,heightRect);
-    stroke(0);
-    x += segmentLength;
-  } 
+    //Draw flag at tallest
+    // tallest = heightRect+2; 
+    
+    // if(tallest > heightRect){
+    //   drawFlag(x,heightRect);
+    // } 
+    
+    //Average
+    print(round(Math.floor(heightRect)));
+    
 
+    //Render the line
+    // stroke("red");
+    // line(0,tallest,width,tallest);
+    // stroke(0);
+    
+  } 
+  print("width",width);
 
 }
 
 function drawFlag(positionX,positionY){
-  rect()
+  rectMode(CORNER);
+  stroke("red");
+  rect(positionX,positionY,1,10);
+  triangle(positionX,positionY,positionX+2,positionY+2,positionX+4,positionY+5);
+  stroke(0);
+  rectMode(CORNERS);
 }
 
 
@@ -45,9 +66,9 @@ function drawFlag(positionX,positionY){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  randomTerrain();
 }
 
 function draw() {
-  randomTerrain();
+  
 }
