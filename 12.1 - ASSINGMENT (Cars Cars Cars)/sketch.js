eb766a529e04a789b44d48c6fdef068477137cc5
vladatam <vladyslav.atam@gmail.com>
2023-10-19 11:24:23 -0600

    Oct 19

--- 12.1 - ASSINGMENT (Cars Cars Cars)/sketch.js
+++ 12.1 - ASSINGMENT (Cars Cars Cars)/sketch.js
@@ -23,22 +23,26 @@ function setup() {
 }
 
 function mouseClicked(){
-  let randomLocation = int(random(50,150));
-
-  driveRight.push(new Vehicle(0, height/2 + randomLocation, int(random(2)), 1));
-  driveLeft.push(new Vehicle(0, height/2 - randomLocation, int(random(2)), 1));
+  let randomLocation = int(random(50,100));
+  let randomDirection = int(random(2));
+  if(randomDirection === 1){
+    driveLeft.push(new Vehicle(0, height/2 - randomLocation, int(random(2)), 1));
+  }
+  else{  
+  
+    driveRight.push(new Vehicle(0, height/2 + randomLocation, int(random(2)), 1));
+  }
+  
 }
 
 function draw() {
   background(220);
   drawRoad();
   for(let i = 0; i < driveRight.length; i ++){
-    driveRight[i].display();
-    driveRight[i].move();
+    driveRight[i].action();
   }
   for(let i = 0; i < driveLeft.length; i ++){
-    driveLeft[i].display();
-    driveLeft[i].move();
+    driveLeft[i].action();  
   }
   
 }
@@ -62,24 +66,40 @@ function drawRoad(){  //Display a road on screen
 }
 
 class Vehicle{        //Create a vehicle class
-  constructor(x,y,direction,xSpeed){
+  constructor(x,y,direction){
     this.type = int(random(2)), 
     this.x = x, 
     this.y = y;
     this.direction = direction;
-    this.speed = xSpeed;
+    this.speed = random(5);
     this.start = int(random(150));
-
   }
 
+  action(){
+    this.move();
+    this.display();
+    this.speedUp();
+    this.speedDown();
+  }
+  
   speedUp(){
     this.speed += 1; 
     if(this.speed > 15){   //max value for speed. 
       this.speed = 15; 
     }
   }
+  
+  speedDown(){
+    this.speed -= 1;
+    if(this.speed < 0){
+      this.speed = 0;
+    }
+    
+  }
     
   move(){
+    this.speedUp();
+    this.speedDown();
     if(this.x > width){
       this.x = 0;
     }
@@ -87,7 +107,6 @@ class Vehicle{        //Create a vehicle class
       this.x = width;
     }
   }
-
 
   display(){
     this.direct();
@@ -116,13 +135,9 @@ class Vehicle{        //Create a vehicle class
     }
     if(this.direction === 0){  //0 = cars driving right 
       carFacing = carImageR;
-      truckFacing = truckImageR;           
+      truckFacing = truckImageR;          
       this.x += this.speed;      //updated to drive right. 
     }
   }
 }
 
-function keyPressed(){
-  this.speedUp();
-  print(key);
-}
