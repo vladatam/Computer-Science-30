e95fc8d23a5d64a10bfc62f7a6f479155dbd31f5
vladatam <vladyslav.atam@gmail.com>
2023-10-19 10:59:36 -0600

    OCT 19

--- 12.1 - ASSINGMENT (Cars Cars Cars)/sketch.js
+++ 12.1 - ASSINGMENT (Cars Cars Cars)/sketch.js
@@ -11,6 +11,8 @@ let currentCar;
 let driveRight = [];
 let driveLeft = [];
 
+let carFacing, truckFacing; 
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,18 +20,27 @@ function setup() {
   carImageR = loadImage("assets/CarRight.png");
   truckImageL = loadImage("assets/TruckLeft.png");
   truckImageR =loadImage("assets/TruckRight.png");
-  car = new Vehicle(1, width/2, height/2, 1, 2);
-  truck = new Vehicle(0, width/2, height/2);
-  
+}
+
+function mouseClicked(){
+  let randomLocation = int(random(50,150));
+
+  driveRight.push(new Vehicle(0, height/2 + randomLocation, int(random(2)), 1));
+  driveLeft.push(new Vehicle(0, height/2 - randomLocation, int(random(2)), 1));
 }
 
 function draw() {
   background(220);
   drawRoad();
+  for(let i = 0; i < driveRight.length; i ++){
+    driveRight[i].display();
+    driveRight[i].move();
+  }
+  for(let i = 0; i < driveLeft.length; i ++){
+    driveLeft[i].display();
+    driveLeft[i].move();
+  }
   
-  car.move();
-  car.display();
-  truck.display();
 }
 
 function drawRoad(){  //Display a road on screen
@@ -51,29 +62,24 @@ function drawRoad(){  //Display a road on screen
 }
 
 class Vehicle{        //Create a vehicle class
-  constructor(type,x,y,direction,xSpeed){
-    this.type = type, this.x = x, this.y = y;
-    this.direction = direction; this.speed = xSpeed;
+  constructor(x,y,direction,xSpeed){
+    this.type = int(random(2)), 
+    this.x = x, 
+    this.y = y;
+    this.direction = direction;
+    this.speed = xSpeed;
+    this.start = int(random(150));
 
   }
 
   speedUp(){
-    if(this.direction === 1){
-      this.speed += 1;
-    }
-    if(this.direction === 0){
-      this.speed = -1;
-    }
-    if(this.speed > 15){
-      this.speed = 15;
-    }
-    if(this.speed < -15){
-      this.speed = -15;
+    this.speed += 1; 
+    if(this.speed > 15){   //max value for speed. 
+      this.speed = 15; 
     }
   }
     
   move(){
-    this.x += this.speed;
     if(this.x > width){
       this.x = 0;
     }
@@ -84,18 +90,39 @@ class Vehicle{        //Create a vehicle class
 
 
   display(){
-    //Above Right hand Side of the road direction = 1
-    if(this.type === 0 && this.direction === 1){
-      image(carImageR, this.x, this.y + 50, 100, 100);
-    } 
-    if(this.type === 1 && this.direction === 1){
-      image(truckImageR, this.x, height/2 + 50, 100,100);
+    this.direct();
+    if(this.type === 0){   // 0 = car
+      this.drawCar();
+    }
+  
+    if(this.type === 1){  // 1 = truck 
+      this.drawTruck();
     }
   }
-  
+
+  drawCar(){
+    image(carFacing, this.x, this.y, 100, 100);
+  }
+
+  drawTruck(){
+    image(truckFacing, this.x, this.y, 100, 100);
+  }
+
+  direct(){
+    if(this.direction === 1){ //1 = left draw cars driving left
+      carFacing = carImageL;
+      truckFacing = truckImageL;
+      this.x -= this.speed;  //updates to drive to left 
+    }
+    if(this.direction === 0){  //0 = cars driving right 
+      carFacing = carImageR;
+      truckFacing = truckImageR;           
+      this.x += this.speed;      //updated to drive right. 
+    }
+  }
 }
 
 function keyPressed(){
-  car.speedUp();
+  this.speedUp();
   print(key);
 }
