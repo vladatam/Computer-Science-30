92585f9051ec803492392e2e2e55bda55c54a7e6
vladatam <vladyslav.atam@gmail.com>
2023-10-30 12:16:29 -0600

    Progress 2 (Halfway)

--- 14.3 - ASSINGMENT Generative Art/sketch.js
+++ 14.3 - ASSINGMENT Generative Art/sketch.js
@@ -6,22 +6,39 @@
 
 //Global Variables
 
-let offset = 100;
+let offset = 0.3;
+let gridSize = 20;
 
 function setup() {
   createCanvas(800, 800);
   background(220);
-  translate(width*0.55,height*0.55);
-  revolvedLines(width,height);
+
+  // revolvedLines(width,height);
+  grid();
 }
 
 function revolvedLines(x,y){
+  push();
+  
+  translate(width*0.55,height*0.55);
   for(let i = 0; i <1000; i++){
     rotate(radians(i));
     translate(-i,+i);
-    line(0,0,x,0);
+    line(0,0,x,50);
   }
-  
+  pop();
+}
+
+function grid(){
+  colorMode(HSB);
+  for(let x = 0; x<width; x+=gridSize){
+    for(let y = 0; y<height; y+=gridSize){
+      fill(random(y*offset,x*offset), 100,250);
+      circle(x,y,15);
+
+
+    }
+  }
 }
 
 function keyPressed(){
