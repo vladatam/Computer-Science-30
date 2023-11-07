7e55ff0aa5a68b36e37925527ea2feff96be88f8
vladatam <vladyslav.atam@gmail.com>
2023-11-07 12:16:36 -0600

    Puzzle Game Assingment

--- 08.1 - Terrain Generation/sketch.js
+++ 08.1 - Terrain Generation/sketch.js
@@ -3,46 +3,51 @@
 // Vlad Atamanchuk
 //  Oct 5, 2023 
 //
+//Creating a randomly generate terrain using perlin noise to make a smoother transition between values. 
 
 //Global Variables
 
-let segmentLength = 1;
-let lineWidth =1.5;
+let segmentLength = 1; //space between rectangles
+let lineWidth =1; //Stroke weight 
 let x = 0;
-const noiseShift = 0.01;
-let numSegments; 
-let tallestRect =0;
+const noiseShift = 0.01;  
+let numSegments; //Number of rectangles drawn
+let tallestRect =0; 
 let currentRectX,currentRectY;
 
-
 function calculateTerrain(){
-  rectMode(CORNERS);
+  rectMode(CORNERS); //Draw the rectangles from corner to corner
   strokeWeight(lineWidth);
 
 
   let noiseReduction = height; 
+
   let averageCalc = 0;
   let totalHeight =0; 
 
-  for(let i=0; i < width; i++){
-    numSegments = i;
-    x += segmentLength;
+  for(let i=0; i < width; i++){  //Loop until edge of screen to create mountains
+    numSegments = i; 
+    x += segmentLength;   
 
-    let heightRect = random(height);
+    let heightRect = random(height); //Generate a random height for each rectangle 
     
-    //Noise reduction
+    //Noise reduction between the rectangle heights 
     heightRect = noise(noiseReduction); // 0 to 1
     heightRect = map(heightRect,0,1,0,height);
     noiseReduction += noiseShift; 
 
-    //Find tallest rectangle
+    //Need to fix. 
+    //Find tallest rectangle (Error is assuming Y coordinate will be larger, it will be smaller as p5 increases the y
+    //coordinate the lower it is down the screen. This technically need to look for lowes y coordinate to figure out the tallest peak)
+    
     currentRectY = heightRect;
     if(currentRectY > tallestRect){
       tallestRect = currentRectY; 
       currentRectX = numSegments;
       tallestRect -= height;
       tallestRect *=-1;
-      print('x', currentRectX, 'y', tallestRect);
+      //Test
+      // print('x', currentRectX, 'y', tallestRect);
     }
     //Render the mountains
     rect(x,height,x,heightRect);
@@ -50,13 +55,12 @@ function calculateTerrain(){
     //Calculate the average
     totalHeight += heightRect;
     averageCalc = totalHeight/numSegments;  
-    
 
-    
   } 
-
+  //FIX
   //Put flag at tallest
-  print('FINAL x', currentRectX, 'y', tallestRect);
+  //TEST
+  // print('FINAL x', currentRectX, 'y', tallestRect);
   drawFlag(currentRectX,tallestRect);
 
   //Render the line at average
@@ -64,12 +68,12 @@ function calculateTerrain(){
   strokeWeight(2);
   line(0,averageCalc,width,averageCalc);
   stroke(0);
-
-  
+ 
 } 
 
 
 function drawFlag(positionX,positionY){
+  //Render the flag 
   positionY -=20;
   rectMode(CORNER);
   fill("red");
@@ -77,12 +81,10 @@ function drawFlag(positionX,positionY){
   rect(positionX,positionY,4,20);
   triangle(positionX+4,positionY+10,positionX+14,positionY+10,positionX+4,positionY);
   fill(0);
-  rectMode(CORNERS);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
 }
 
 function draw() {
---
+++ 17 - Puzzle Game Starter Demo/sketch.js
@@ -0,0 +1,94 @@
+// Puzzle Game Starter
+// Vlad Atamanchuk
+// Nov 6, 2023
+// A first foray into working with 2D arrays
+
+let grid =
+[[255,  0,  255,   0,  255],
+ [0,   0,    0,    255,  0  ],
+ [255, 255,  0,    255,  0  ],
+ [255, 0,    0,    255,  0 ]];
+
+const NUM_ROWS = 4;  const NUM_COLS = 5;
+let rectWidth, rectHeight, row, col;
+
+function setup() {
+  rectWidth = 70;  rectHeight = 70;
+  createCanvas(NUM_COLS*rectWidth, NUM_ROWS*rectHeight); 
+  
+}
+
+function draw() {
+  row = getCurrentY();   col = getCurrentX();
+  background(220);
+  renderGrid();
+  winner();
+  print(col,row); //prints x,y
+}
+
+function mousePressed(){
+  //when the mouse is clicked flip the value at current
+  //col,row + also flip 4 cardinal neighbours (N,E,S,W)
+
+  //flip the 4 neighbours up,down,left,right
+  if(keyIsPressed){
+    if(key === "Shift")flip(col,row); //flip only @ mouse position if shift is held.
+  }
+  else {
+    flip(col,row);
+    if(col < NUM_COLS -1) flip(col+1,row); //Right Neighbour
+    if(row>0)flip(col,row-1); //Up Neighbour
+    flip(col-1,row);
+    if(row < NUM_ROWS-1) flip(col,row+1);
+  }
+  
+}
+
+function flip(col,row){
+  //at a given x,y flip the value in the 2D array. 
+  //0=225, 225 =0
+  if(grid[row][col] === 0)grid[row][col] =255;
+  else grid[row][col] = 0;
+  
+}
+
+function winner(){
+  let check;
+  print(grid)
+  print(grid.every(checkColour));
+  if(grid === 0){
+    textAlign(CENTER);
+    fill('red');
+    print('win');
+    textSize(20);
+    text("You Win!",width/2,height/2);
+  }
+  
+}
+
+function checkColour(colour){
+  return colour < 1;
+}
+
+function getCurrentX(){ //determine current column mouse is in, and return
+  let constrainMouseX = constrain(mouseX, 0, width-1);
+  return floor(constrainMouseX/rectWidth);  
+}
+function getCurrentY(){ //determine current row mouse is in, and return
+  let constrainMouseY = constrain(mouseY, 0, height-1);
+  return floor(constrainMouseY/rectHeight);
+}
+
+function renderGrid(){
+  //creates a 2D grid of squares using information from our
+  //2D array for the corresponding fill values
+  for(let x = 0; x < NUM_COLS; x++){
+    for(let y = 0; y < NUM_ROWS; y++){
+      let fillValue = grid[y][x];
+      fill(fillValue);
+      //x:    0,   1,   2,  3,   4
+      //posx  0   50, 100, 150,200   expression? x→posx
+      rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
+    }
+  }
+}
