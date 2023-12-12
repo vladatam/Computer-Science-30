3e38487a644e86e369f95f329f1b0a901596e13e
vladatam <vladyslav.atam@gmail.com>
2023-12-12 12:17:16 -0600

    Day 4 - Figuring out how to use arrays to store multiple images
    Today was very low progress, program was not running as there are errors. Need to fix this issue by figuring out how to use the arrays to scroll through the images.

--- MAJOR CAPSTONE PROJECT/sketch.js
+++ MAJOR CAPSTONE PROJECT/sketch.js
@@ -9,10 +9,12 @@
 let balls = [];
 let catcher;
 let ground, wall, wall2;
-let picture;
+let pictures = [];
+let currentImage = 0; 
 
 function preload(){
-  picture = loadImage("assets/Diamond.png");
+  pictures.push = loadImage("assets/Diamond.png");
+  pictures.push = loadImage("assets/Strawberry.png");
 }
 
 function setup() {
@@ -40,12 +42,12 @@ function setup() {
 
 function draw() {
   background(200);
-
+  overlay();
   if (mouse.pressed()) {
     let ball = createSprite(mouseX, mouseY, 20, 20);
-    ball.img = picture;
-    ball.scale = 0.5;
-    ball.d = 200;
+    ball.img = pictures[currentImage];
+    ball.scale = 0.20;
+    ball.d = 80;
     ball.velocity.y = 5;
     ball.rotation = 2;
     ball.shapeColor = color(random(255), random(255), random(255));
@@ -64,10 +66,16 @@ function draw() {
   }
 
   if(kb.pressing(LEFT_ARROW)) {
-    catcher.position.x -= 5;
+    catcher.position.x -= 20;
   } else if (kb.pressing(RIGHT_ARROW)) {
-    catcher.position.x += 5;
+    catcher.position.x += 20;
   }
 
   drawSprites();
+}
+
+
+function overlay(){
+  imageMode(CENTER);
+  image(pictures[currentImage], mouseX, mouseY, 125, 100);
 }
