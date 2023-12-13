f8b2747720c1896798ec812c101fb64df36f4cb9
vladatam <vladyslav.atam@gmail.com>
2023-12-13 12:18:50 -0600

    Day 5 - A Bit of a set back
    Today was a set back as I figured that it would be better to implement classes into this assingment, however its quite difficult learning how to use the sprites with classes so today had to rewrite almost all of my code, but i think this should be helpful in the future. I also decided to make it easier for myself and figure out how to combine just the balls themselves, before i figure out how to combine images.

--- MAJOR CAPSTONE PROJECT/sketch.js
+++ MAJOR CAPSTONE PROJECT/sketch.js
@@ -7,75 +7,79 @@
 
 //Global Variables
 let balls = [];
-let catcher;
 let ground, wall, wall2;
 let pictures = [];
+let picture; 
 let currentImage = 0; 
 
+let diameter = 40; 
+
 function preload(){
-  pictures.push = loadImage("assets/Diamond.png");
-  pictures.push = loadImage("assets/Strawberry.png");
+  //picture = loadImage("assets/Diamond.png");
+  //= loadImage("assets/Strawberry.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  balls.push(new Balls);
   stroke("white");
 
   world.gravity.y = 2;
 
   ground = createSprite(width / 2, height - 10, width, 20);
-  ground.immovable = true;
+  ground.static = true;
   ground.color = "white";
 
   wall = createSprite(0, 0, 40, height*2);
-  wall.immovable = true;
+  wall.static = true;
   wall.color = "white";
 
   wall2 = createSprite(width, 0, 40, height*2);
-  wall2.immovable = true;
+  wall2.static = true;
   wall2.color = "white";
   
-  catcher = createSprite(width / 2, height - 40, 80, 20);
-  catcher.shapeColor = color(255);
-  catcher.collider = "k";
-}
+  }
 
 function draw() {
   background(200);
-  overlay();
-  if (mouse.pressed()) {
-    let ball = createSprite(mouseX, mouseY, 20, 20);
-    ball.img = pictures[currentImage];
-    ball.scale = 0.20;
-    ball.d = 80;
-    ball.velocity.y = 5;
-    ball.rotation = 2;
-    ball.shapeColor = color(random(255), random(255), random(255));
-    balls.push(ball);
+  for(let i = 0; i < balls.length; i++){
+    balls[i].create();
+    balls[i].display();
+    balls[i].overlay();
   }
-
-  for (let i = 0; i < balls.length; i++) {
-    let ball = balls[i];
-    if (ball.collide(ground)) {
-      ball.velocity.y = 0;
-    }
-
-    if (catcher.overlap(ball)) {
-      ball.remove();
-    }
-  }
-
-  if(kb.pressing(LEFT_ARROW)) {
-    catcher.position.x -= 20;
-  } else if (kb.pressing(RIGHT_ARROW)) {
-    catcher.position.x += 20;
-  }
-
-  drawSprites();
+  
+  
 }
 
+class Balls {
+  constructor(){
+    this.diameter = 20; 
+    this.velocity = 5; 
+    this.rotation = 2;
+    this.shapeColor = color('red');
+  }
+  
+  create(){
+    if (mouse.pressed()) {
+      let ball = createSprite(mouseX, 200, 20, 20);
+      //ball.img = picture;
+      ball.scale = 0.20;
+      ball.d = diameter;
+      ball.velocity.y = 5;
+      ball.rotation = 2;
+      ball.shapeColor = color(random(255), random(255), random(255));
+      balls.push(ball);
+    }
+  }
 
-function overlay(){
-  imageMode(CENTER);
-  image(pictures[currentImage], mouseX, mouseY, 125, 100);
+  display(){
+    drawSprites();  //Draw all the sprites. 
+  }
+
+  overlay(){
+    //Overlay of where the ball will be dropped. 
+    circle(mouseX, 200, diameter); 
+    line(mouseX, 200, mouseX, height);
+  }
+
 }
