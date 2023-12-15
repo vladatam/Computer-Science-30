a82f9588b6e19b6c9b13e8089d198d8d61c73fb7
vladatam <vladyslav.atam@gmail.com>
2023-12-15 12:17:40 -0600

    Day 6 - Again not much progress just testing things out
    Added a new colour when the balls merge, and testing to see how to only merge balls with the same diameter.

--- TESTING/sketch.js
+++ TESTING/sketch.js
@@ -7,6 +7,7 @@ let currentImage = 0;
 
 let diameter = 40; 
 
+let borders; 
 
 
 function preload(){
@@ -20,25 +21,27 @@ function setup() {
 
   world.gravity.y = 2;
 
+
   balls = new Group();
 
-  ground = createSprite(width / 2, height - 10, width, 20);
-  ground.static = true;
-  ground.color = "white";
+  balls.colour = "green";
 
-  wall = createSprite(0, 0, 40, height*2);
-  wall.static = true;
-  wall.color = "white";
+  borders = new Group();
+  borders.add = new borders.Sprite(width / 2, height - 10, width, 20); //Floor
+  borders.add = new borders.Sprite(0, 0, 40, height*2); //Left Wall 
+  borders.add = new borders.Sprite(width, 0, 40, height*2); //Right Wall
+  borders.static = true;
+  borders.color = "white";
 
-  wall2 = createSprite(width, 0, 40, height*2);
-  wall2.static = true;
-  wall2.color = "white";
-  }
+  borders.add = new borders.Sprite(0, 100, width*2, 1);
+  borders[3].static = false;
+}
 
 function draw() {
   background(200);
   overlay();
-  drawSprites();
+  borders.draw();
+  balls.draw();
   for(let i=0; i<balls.length; i++){
     mergeBalls(balls[i]);
   }
@@ -46,29 +49,32 @@ function draw() {
 
 function mousePressed(){
   let ball = new balls.Sprite(mouseX, 100, 20, 20);
-    //ball.img = picture;
+  //ball.img = picture;
   ball.d = 50;
   ball.velocity.y = 2;
   ball.rotation = 2;
   ball.shapeColor = "red";
   print(balls);
  
-
 }
 
-function mergeBalls(ball){
-  for(let i = 0; i<balls.length; i++){
-    if(balls[i].collides(balls)){
-      balls[i+1].diameter += balls[i].diameter/2;
+function mergeBalls(){
+  for(let i = 0; i<balls.length-1; i++){
+    if(balls[i].collides(balls[i+1])){
+      balls[i+1].diameter += balls[i].diameter/3;
       balls[i+1].position.x = balls[i].position.x;
       balls[i+1].position.y = balls[i].position.y;
-      balls[i+1].colour = "blue";
+      balls[i+1].color = "blue";
+      
       balls[i].remove();
     }
-  }
+    
+    
   
-}
+  
+  }
 
+}
 
 function overlay(){
   circle(mouseX, 100, 50);
