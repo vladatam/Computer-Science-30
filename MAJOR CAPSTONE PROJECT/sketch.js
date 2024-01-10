67d362c66913446ddf63c304296d1e1ce711a01f
vladatam <vladyslav.atam@gmail.com>
2024-01-10 12:17:42 -0600

    Day 13 - Added so fruits arent being spawned outside the canvas
    Today just made sure to not spawn any fruit when the mouse is over the borders, and tried to figure out the color index, still need to figure that out.

--- MAJOR CAPSTONE PROJECT/sketch.js
+++ MAJOR CAPSTONE PROJECT/sketch.js
@@ -8,29 +8,29 @@
 //Global Variables
 let score; 
 let balls;
+
+
 let button;
 let ballManager; 
 let borders;
 let pictures = [];
 let picture; 
-let currentImage = 0; 
+
 
 let currentScore = 0; 
 
 let imageIndex = 0;
-
-let diameter = 40; 
+let currentImage = 0; 
 
 let gameOver = false; 
-
 
 
 function preload(){
   pictures.push(loadImage("assets/plum.png"));
   pictures.push(loadImage("assets/apple.png"));
   pictures.push(loadImage("assets/pear.png"));
-  pictures.push(loadImage("assets/tomato.png"));
   pictures.push(loadImage("assets/mango.png"));
+  pictures.push(loadImage("assets/tomato.png"));
 }
 
 function setup() {
@@ -45,7 +45,7 @@ function setup() {
 
   balls = new Group();
 
-  world.gravity.y = 2; //Gravity so the balls fo
+  world.gravity.y = 5; //Gravity so the balls fo
 
   button = createSprite(50,50, 50, 20);
   button.text = "Restart";
@@ -70,12 +70,16 @@ function draw() {
   }
   else balls.debug = false; 
   
-  
 }
 
 function mousePressed(){ 
   if(!gameOver){
+    if(!borders[1].mouse.hovering() && !borders[2].mouse.hovering()){ //See if mouse is in the playing area. 
+      imageIndex = floor(random(4));
+      currentImage = imageIndex; 
+      print(currentImage, imageIndex);
     ballManager.create(); 
+    }
   }
   
 }
@@ -89,8 +93,6 @@ class Balls {
   
 
   setUpBorders(){
-
-
     borders.add = new borders.Sprite(width / 2, height-20, width, 50); //Floor
     borders.add = new borders.Sprite(0, 0, width*0.45, height*2); //Left Wall 
     borders.add = new borders.Sprite(width, 0, width*0.45, height*2); //Right Wall
@@ -100,12 +102,19 @@ class Balls {
   }
   
   create() {
-    this.diameter = 20;
-    imageIndex = 0;
+
+    this.diameter = floor(random([20,30,40,50,60]));
+    if(this.diameter === 20) imageIndex = 0;
+    if(this.diameter === 30) imageIndex = 1; 
+    if(this.diameter === 40) imageIndex = 2;
+    if(this.diameter === 50) imageIndex = 3;
+    if(this.diameter === 60) imageIndex = 4;
+    currentImage = imageIndex
+    print(this.diameter, imageIndex);
     let ball = createSprite(mouseX, 200, this.diameter, this.diameter);
     ball.img = pictures[imageIndex];
-    ball.scale = this.diameter /100;// Adjust scale based on diameter
-    ball.d = this.diameter ; // Set the diameter for the ball
+    ball.scale = this.diameter / 100; // Adjust scale based on diameter
+    ball.d = this.diameter;// Set the diameter for the ball
     ball.velocity.y = this.velocity;
     ball.rotation = this.rotation; // Initial color
     balls.add(ball);
@@ -117,14 +126,18 @@ class Balls {
       for (let j = i + 1; j < balls.length; j++) {
         if (balls[i].collides(balls[j])) {
           if (floor(balls[j].diameter) === floor(balls[i].diameter)) {
-            balls[j].diameter += balls[i].diameter/2 ;
+            balls[j].diameter += balls[i].diameter/4;
+            print(balls[j].diameter);
             currentScore += this.diameter / 2;
             balls[j].position.x = balls[i].position.x;
             balls[j].position.y = balls[i].position.y;
-            balls[j].scale = balls[j].diameter / 100;
-            imageIndex ++;
-            print(imageIndex);
-            // Assigning a different color for each new diameter
+            balls[j].scale = balls[j].d / 100;
+
+            if (balls[j].d > 20 && balls[j].d < 30) imageIndex = 0;
+            else if (balls[j].d > 30 && balls[j].d < 40) imageIndex = 1;
+            else if (balls[j].d > 40 && balls[j].d < 50) imageIndex = 2;
+            else if (balls[j].d > 50 && balls[j].d < 60) imageIndex = 3;
+            else if (balls[j].d > 60 && balls[j].d < 70) imageIndex = 4;
             balls[j].img = pictures[imageIndex];
             
             balls[i].remove();
@@ -137,8 +150,10 @@ class Balls {
 
   overlay(){
     //Overlay of where the ball will be dropped. 
-    circle(mouseX, 200,this.diameter); 
+    imageMode(CENTER);
+    image(pictures[currentImage], mouseX, 200, this.diameter, this.diameter); 
     line(mouseX, 200, mouseX, height);
+    image(pictures[currentImage], width* 0.70, 100, this.diameter, this.diameter );
   }
 
   losingLine() {
@@ -156,7 +171,7 @@ class Balls {
   score(){
     textAlign(CENTER);
     textSize(40);
-    text(currentScore, 200, height/2);
+    text(currentScore, width/2, 150);
   }
   
 
